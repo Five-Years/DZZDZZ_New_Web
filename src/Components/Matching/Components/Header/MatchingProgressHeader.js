f62b67f989da42caad4198f23a552b8883a09fa1
Jiinsung <crm03008@gmail.com>
2023-06-25 02:06:35 +0900

    매칭뷰 수정

--- src/Components/Matching/Components/Header/MatchingProgressHeader.js
+++ src/Components/Matching/Components/Header/MatchingProgressHeader.js
@@ -1,59 +1,78 @@
-import React from 'react'
+import React from "react";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 import { ReactComponent as Mile } from "../../../../assets/mile.svg";
 
-
 function MatchingProgressHeader(props) {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     // <ContentContainers>      </ContentContainers>
 
-      <>
-        <ContentLeft>
-          {props.isFirst? <></> : <><ArrowBackIosIcon
-            style={{ marginLeft: "15.4%", width: "60%", height: "60%" }}
-            onClick={() => {navigate("/");
-            }}
-          /></> }
-        </ContentLeft>
-        <ContentTitle>
-          <text></text>
-        </ContentTitle>
-        <ContentRight>
-          {props.isReport?<><MoreHorizIcon onClick={()=>{window.ReactNativeWebView?.postMessage(
+    <>
+      <ContentLeft>
+        {props.isFirst ? (
+          <></>
+        ) : (
+          <>
+            <ArrowBackIosIcon
+              style={{ marginLeft: "15.4%", width: "60%", height: "60%" }}
+              onClick={() => {
+                navigate("/Matching");
+              }}
+            />
+          </>
+        )}
+      </ContentLeft>
+      <ContentTitle>
+        <text></text>
+      </ContentTitle>
+      <ContentRight>
+        {props.isReport ? (
+          <>
+            <MoreHorizIcon
+              onClick={() => {
+                window.ReactNativeWebView?.postMessage(
                   JSON.stringify({ type: "report", data: "" })
-                ); alert("신고")}}
-            style={{ width: "50%", height: "50%", marginRight: "15.4%" }}
-          /></> : <><MileHeader><Mile /><text>1</text></MileHeader></>}
-        </ContentRight>
-        </>
-  )
+                );
+                alert("신고");
+              }}
+              style={{ width: "50%", height: "50%", marginRight: "15.4%" }}
+            />
+          </>
+        ) : (
+          <>
+            <MileHeader>
+              <Mile />
+              <text>1</text>
+            </MileHeader>
+          </>
+        )}
+      </ContentRight>
+    </>
+  );
 }
 
-export default MatchingProgressHeader
-
+export default MatchingProgressHeader;
 
 const MileHeader = styled.div`
-display: flex;
-width: 50%;
-height: 100%;
-margin-right : 15.4%;
-align-items: center;
-justify-content: space-evenly;
-z-index: 1;
+  display: flex;
+  width: 50%;
+  height: 100%;
+  margin-right: 15.4%;
+  align-items: center;
+  justify-content: space-evenly;
+  z-index: 1;
 
-> text {
-  font-family: var(--font-OpenSans);
-  font-size: 16px;
-  font-weight: 600;
-  line-height: 24px;
-  letter-spacing: 0em;
-  text-align: left;
-
-}
+  > text {
+    font-family: var(--font-OpenSans);
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 24px;
+    letter-spacing: 0em;
+    text-align: left;
+  }
 `;
 
 export const ContentContainers = styled.div`
@@ -68,8 +87,6 @@ export const ContentContainers = styled.div`
 
   /* border-bottom: 0.3px solid #888888; */
 `;
-
-
 
 export const ContentLeft = styled.div`
   display: flex;
@@ -94,18 +111,18 @@ export const ContentTitle = styled.div`
 
   > text {
     font-family: var(--font-OpenSans);
-  font-style: normal;
-  font-weight: 400;
-  font-size: 19px;
-  line-height: 22px;
-  /* identical to box height, or 116% */
+    font-style: normal;
+    font-weight: 400;
+    font-size: 19px;
+    line-height: 22px;
+    /* identical to box height, or 116% */
 
-  text-align: center;
-  letter-spacing: -0.408px;
+    text-align: center;
+    letter-spacing: -0.408px;
 
-  /* Text Black */
+    /* Text Black */
 
-  color: #000000;
+    color: #000000;
   }
 `;
 
